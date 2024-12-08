01c803d9d82270b623e9bfe20dd297bc1eb7cfdc
arnab-156 <vendmyt@gmail.com>
2024-12-08 14:15:50 -0500

    navigation tab fixed

--- src/app/components/modal.tsx
+++ src/app/components/modal.tsx
@@ -10,11 +10,11 @@ interface ModalProps {
 export const Modal: NextPage<ModalProps> = ({ onClose, children, title }) => (
     <div id="default-modal" aria-hidden="true" className={`h-screen w-screen overflow-y-auto overflow-x-hidden 
     fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-        <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="relative p-4 w-full h-full">
             {/* <!-- Modal content --> */}
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-full h-full">
                 {/* <!-- Modal header --> */}
-                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 h-[10vh]">
                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                         {title}
                     </h3>
@@ -29,11 +29,11 @@ export const Modal: NextPage<ModalProps> = ({ onClose, children, title }) => (
                     </button>
                 </div>
                 {/* <!-- Modal body --> */}
-                <div className="p-4 md:p-5 space-y-4">
+                <div className="p-4 md:p-5 space-y-4 h-[80vh] overflow-y-scroll">
                     {children}
                 </div>
                 {/* <!-- Modal footer --> */}
-                <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 h-[10vh]">
                     <button tabIndex={1} type="button" className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:invert">Continue</button>
                     <button tabIndex={1} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={onClose}>Close</button>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Modal } from "./components/modal";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalTitle, setModalTitle] = useState<string>("");
   const gridDesign = [
     [0, "about", "blogs", "home"],
     [0, "social", "interviews", 0],
@@ -14,18 +15,25 @@ export default function Home() {
 
   const $blobClassName = `block bg-black rounded-full scale-x-90 scale-y-90 p-2 md:p-4 hover:scale-100 transition delay-100 duration-700 ease-in-out w-[25vw] h-[25vw] lg:max-h-[25vh] lg:max-w-[25vh] content-center hover:text-white focus:text-white focus:shadow-2xl focus:shadow-indigo-500/50`;
 
+  const handleOnClick = (isOpen: boolean, title: string | number) => {
+    if (typeof title === 'string') {
+      setModalTitle(title);
+    }
+    setIsModalOpen(!isOpen);
+  };
 
   // modal component is needed. 1. right now simple open close with click.
   // based on input, finds from library and shows modal.
 
+
   return (
     <main className="h-screen m-auto">
-      <DesktopNav />
+      <DesktopNav isModalOpen={isModalOpen} func={handleOnClick} />
       <div className="h-3/4 grid grid-cols-4 grid-flow-row gap-4 pt-[20%] md:pt-[6rem] lg:max-h-fit">
         {
           gridDesign.map((innerArr, id) => innerArr.map((elem, idx) => (
             <button
-              onClick={() => setIsModalOpen(!isModalOpen)}
+              onClick={() => handleOnClick(isModalOpen, elem)}
               tabIndex={isModalOpen ? -1 : 0}
               type="button"
               data-modal-target="default-modal" data-modal-toggle="default-modal"
@@ -40,8 +48,8 @@ export default function Home() {
       {
         isModalOpen && (
           <Modal
-            title="CURRENT BUTTON CLICK"
-            onClose={() => setIsModalOpen(!isModalOpen)}
+            title={modalTitle}
+            onClose={() => setIsModalOpen(false)}
           >
             <p>Content Coming Soon.</p>
           </Modal>)
